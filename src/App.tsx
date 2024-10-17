351cb56e960352310456ad2792093c72bf6fa630
sivaTechie-16 <siva1p21mc046@gmail.com>
2024-10-17 12:11:44 +0530

    initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,19 +1,7 @@
-// import { BrowserRouter, Route, Routes } from "react-router-dom"
-import LunchCountTable from "./components/LunchCountTable"
-// import FilterData from "./components/FilterData"
 
 function App() {
-
   return (
-    <>
-    {/* <BrowserRouter>
-    <Routes>
-      <Route path="/" element={<LunchCountTable/>}></Route>
-      <Route path="/filterdata" element={<FilterData/>}></Route>
-    </Routes>
-    </BrowserRouter> */}
-    <LunchCountTable/>
-    </>
+    <div>App</div>
   )
 }
 
